fb413c714789e1dfbe3895ef4ba099c791478aff
Valentina <v.o.butenko@gmail.com>
2016-09-09 11:53:32 +0300

    Set SelectionVC as rootVC

---
+++ SimpleTransitionAnimations/SelectionVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimpleTransitionAnimations
+//
+//  Created by Valentina Butenko on 9/9/16.
+//  Copyright © 2016 ValentinaButenko. All rights reserved.
+//
+
+import UIKit
+
+class SelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.greenColor()
+    }
+}
+

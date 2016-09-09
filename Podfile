bfeb841935dcb6c3eb198c213d81f7ddd7cb4993
Valentina <v.o.butenko@gmail.com>
2016-09-09 11:55:22 +0300

    Install SnapKit

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+
+target 'SimpleTransitionAnimations' do
+  use_frameworks!
+	pod 'SnapKit', '0.30.0.beta2'
+end
---
+++ Pods/Target Support Files/Pods-SimpleTransitionAnimations/Pods-SimpleTransitionAnimations-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleTransitionAnimations : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleTransitionAnimations
+@end
